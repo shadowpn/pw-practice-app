c3d08aef08641215629d40be686f15e98a23870e
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2023-12-17 10:28:59 +0200

    locators in page objects

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,34 +1,45 @@
-import { Page } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 
 export class NavigationPage { //Определяет класс NavigationPage, который будет экспортирован для использования в других частях теста.
     
     readonly page: Page //Объявляет свойство page, которое является экземпляром класса Page из Playwright. Модификатор readonly указывает, что это свойство может быть присвоено только один раз (обычно в конструкторе).
+    readonly formLayoutsMenuItem: Locator
+    readonly datePickerMenuItem: Locator
+    readonly smartTableMenuItem: Locator
+    readonly toastrMenuItem: Locator
+    readonly tooltipMenuItem: Locator
+
     constructor(page: Page) { //Конструктор класса, который принимает объект страницы (Page) и инициализирует свойство page этим объектом.
        this.page = page
+       this.formLayoutsMenuItem = page.getByText('Form Layouts')
+       this.datePickerMenuItem = page.getByText('Datepicker')
+       this.smartTableMenuItem = page.getByText('Smart Table')
+       this.toastrMenuItem = page.getByText("Toastr")
+       this.tooltipMenuItem = page.getByText("Tooltip")
     }
     async formLayoutsPage() { //Асинхронный метод formLayoutsPage, который при вызове выполняет навигацию на страницу форм.
         await this.selectGroupMenuItem('Forms') //Внутри метода formLayoutsPage, сначала находит и кликает по элементу с текстом "Forms", затем по элементу с текстом "Form Layouts" на странице.
-        await this.page.getByText("Form Layouts").click()
+        await this.formLayoutsMenuItem.click()
    
     }
     async datepickerPage() {
     
         await this.selectGroupMenuItem('Forms')
         //await this.page.waitForTimeout(1000)
-        await this.page.getByText('Datepicker').click()   
+        await this.datePickerMenuItem.click()   
 
     }
-    async smartTablePage(){
+    async smartTablePage() {
         await this.selectGroupMenuItem('Tables & Data') //Ищет на странице элемент с текстом 'Tables & Data' и кликает по нему. Это может быть кнопка или ссылка, переводящая пользователя на раздел сайта, связанный с таблицами и данными.
-        await this.page.getByText('Smart Table').click() 
+        await this.smartTableMenuItem.click() 
     }
     async toastrtPage() {
          await this.selectGroupMenuItem("Modal & Overlays") //Ищет на странице элемент с текстом "Modal & Overlays" и кликает по нему. Это обычно используется для перехода на другую часть сайта или открытия модального окна.
-         await this.page.getByText("Toastr").click() //Аналогично, ищет и кликает по элементу с текстом "Toastr". Это может быть действие, которое ведет пользователя к функциональности, связанной с уведомлениями (toasts).
+         await this.toastrMenuItem.click() //Аналогично, ищет и кликает по элементу с текстом "Toastr". Это может быть действие, которое ведет пользователя к функциональности, связанной с уведомлениями (toasts).
     }
     async tooltipPage(){
           await this.selectGroupMenuItem("Modal & Overlays") //Ищет на странице элемент с текстом "Modal & Overlays" и кликает по нему. Это обычно используется для перехода на другую часть сайта или открытия модального окна.
-          await this.page.getByText("Tooltip").click() //Аналогично, ищет и кликает по элементу с текстом "Toastr". Это может быть действие, которое ведет пользователя к функциональности, связанной с уведомлениями (toasts).
+          await this.tooltipMenuItem.click() //Аналогично, ищет и кликает по элементу с текстом "Toastr". Это может быть действие, которое ведет пользователя к функциональности, связанной с уведомлениями (toasts).
     }
     private async selectGroupMenuItem(groupItemTitle: string) { //    private: Этот ключевой слово означает, что метод selectGroupMenuItem является приватным, то есть он может быть вызван только внутри класса, в котором он определен.async: Это ключевое слово указывает на то, что функция асинхронная. Асинхронные функции могут выполнять длительные сетевые запросы, операции с файлами или другие задачи, которые занимают время, без блокировки выполнения кода.
     //selectGroupMenuItem: Это название метода.
