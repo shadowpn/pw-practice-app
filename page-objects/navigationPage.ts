973b2ea5a707249ab3bb27666b435f17efb380f0
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2023-12-17 09:34:11 +0200

    Update navigationPage

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -7,7 +7,35 @@ export class NavigationPage { //Определяет класс NavigationPage, 
        this.page = page
     }
     async formLayoutsPage() { //Асинхронный метод formLayoutsPage, который при вызове выполняет навигацию на страницу форм.
-        await this.page.getByText("Forms").click() //Внутри метода formLayoutsPage, сначала находит и кликает по элементу с текстом "Forms", затем по элементу с текстом "Form Layouts" на странице.
+        await this.selectGroupMenuItem('Forms') //Внутри метода formLayoutsPage, сначала находит и кликает по элементу с текстом "Forms", затем по элементу с текстом "Form Layouts" на странице.
         await this.page.getByText("Form Layouts").click()
+   
+    }
+    async datepickerPage() {
+    
+        await this.selectGroupMenuItem('Forms')
+        //await this.page.waitForTimeout(1000)
+        await this.page.getByText('Datepicker').click()   
+
+    }
+    async smartTablePage(){
+        await this.selectGroupMenuItem('Tables & Data') //Ищет на странице элемент с текстом 'Tables & Data' и кликает по нему. Это может быть кнопка или ссылка, переводящая пользователя на раздел сайта, связанный с таблицами и данными.
+        await this.page.getByText('Smart Table').click() 
+    }
+    async toastrtPage() {
+         await this.selectGroupMenuItem("Modal & Overlays") //Ищет на странице элемент с текстом "Modal & Overlays" и кликает по нему. Это обычно используется для перехода на другую часть сайта или открытия модального окна.
+         await this.page.getByText("Toastr").click() //Аналогично, ищет и кликает по элементу с текстом "Toastr". Это может быть действие, которое ведет пользователя к функциональности, связанной с уведомлениями (toasts).
+    }
+    async tooltipPage(){
+          await this.selectGroupMenuItem("Modal & Overlays") //Ищет на странице элемент с текстом "Modal & Overlays" и кликает по нему. Это обычно используется для перехода на другую часть сайта или открытия модального окна.
+          await this.page.getByText("Tooltip").click() //Аналогично, ищет и кликает по элементу с текстом "Toastr". Это может быть действие, которое ведет пользователя к функциональности, связанной с уведомлениями (toasts).
+    }
+    private async selectGroupMenuItem(groupItemTitle: string) { //    private: Этот ключевой слово означает, что метод selectGroupMenuItem является приватным, то есть он может быть вызван только внутри класса, в котором он определен.async: Это ключевое слово указывает на то, что функция асинхронная. Асинхронные функции могут выполнять длительные сетевые запросы, операции с файлами или другие задачи, которые занимают время, без блокировки выполнения кода.
+    //selectGroupMenuItem: Это название метода.
+    //(groupItemTitle: string): Метод принимает один параметр - groupItemTitle, который является строкой. Это название элемента группового меню, с которым будет взаимодействовать функция.
+        const groupMenuItem = this.page.getByTitle(groupItemTitle) //Объявляется постоянная переменная с именем groupMenuItem. Относится к объекту page текущего экземпляра класса. page обычно является объектом страницы Playwright, который представляет открытую страницу в браузере.
+        const expendedState = await groupMenuItem.getAttribute('aria-expanded')  
+        if(expendedState == "false") //Эта строка проверяет, равно ли значение переменной expendedState (которая содержит значение атрибута aria-expanded) строке "false". Если это так, то код внутри блока if будет выполнен.
+          await groupMenuItem.click()
     }
 }
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -11,4 +11,8 @@ test('navigate to home page', async({page}) => { //Определение тес
     const navigateTo = new NavigationPage(page) //Создает новый экземпляр класса NavigationPage, передавая ему текущий объект page. Этот класс предположительно предназначен для упрощения навигации по страницам веб-приложения.
 
     await navigateTo.formLayoutsPage() //Вызывает метод formLayoutsPage экземпляра navigateTo
+    await navigateTo.datepickerPage()
+    await navigateTo.smartTablePage()
+    await navigateTo.toastrtPage()
+    await navigateTo.tooltipPage()
 })
